5a0dee61ddf4e94179b784f2050f4a066e25f263
Tyler V <verhovectyler@gmail.com>
2022-04-15 00:00:02 -0600

    Added a line that follows the launched projectile

--- Mission Demolition Prototype/Assets/_Scripts/FollowCam.cs
+++ Mission Demolition Prototype/Assets/_Scripts/FollowCam.cs
@@ -18,11 +18,31 @@ public class FollowCam : MonoBehaviour {
     }//Awake
 
     void FixedUpdate () {
-        //If there's only one line following an if, it doesn't need braces
-        if (POI == null) return; //Return if there is no poi
+//      //If there's only one line following an if, it doesn't need braces
+//      if (POI == null) return; //Return if there is no poi
 
-        //Get the position of the poi
-        Vector3 destination = POI.transform.position;
+//      //Get the position of the poi
+//      Vector3 destination = POI.transform.position;
+
+        Vector3 destination;
+        //If there is no poi, return to P:[ 0, 0, 0 ]
+        if (POI == null ) {
+            destination = Vector3.zero;
+        }else {
+            //Get the position of the poi
+            destination = POI.transform.position;
+            //If poi is a Projectile, check to see if it's at rest
+            if (POI.tag == "Projectile" ) {
+                //If it is sleeping (that is, not moving)
+                if ( POI.GetComponent<Rigidbody>().IsSleeping() ) {
+                    //Return to default view
+                    POI = null ;
+                    //In the next update
+                    return ;
+                }
+            }
+        }
+
         //Limit the X & Y to minimum values
         destination.x = Mathf.Max( minXY.x, destination.x );
         destination.y = Mathf.Max( minXY.y, destination.y );
--- Mission Demolition Prototype/Assets/_Scripts/Slingshot.cs
+++ Mission Demolition Prototype/Assets/_Scripts/Slingshot.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
 
+    static private Slingshot S;
+
     //Fields set in the Unity Inspector pane
     [Header("Set in Inspector")]
     public GameObject          prefabProjectile;
@@ -17,7 +19,15 @@ public class Slingshot : MonoBehaviour {
     public bool                aimingMode;
     private Rigidbody          projectileRigidbody;
 
+    static public Vector3 LAUNCH_POS {
+        get {
+            if (S == null ) return Vector3.zero;
+            return S.launchPos;
+        }
+    }//LAUNCH_POS
+
     void Awake() {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
